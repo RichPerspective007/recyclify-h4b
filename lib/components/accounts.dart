7fd24e89ec322caf33606ec94b55ebf0c0d8753d
Sankhadeep-Bhowmik <sankhadeepbhowmik04@gmail.com>
2024-06-29 03:21:34 +0530

    changes at 3:21 am 	new file:   lib/components/accounts.dart 	modified:   pubspec.yaml

---
+++ lib/components/accounts.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class AccountScreen extends StatefulWidget {
+  const AccountScreen({super.key});
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){},icon:Icon(
+
+        ),
+      ),
+      ),
+    );
+    
+    
+  }
+}
