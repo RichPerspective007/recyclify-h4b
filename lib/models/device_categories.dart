12ca7df7d36980af9b69214a4ccf43d3e6fea370
SHIVADITYA BHATTACHARYA <shivadityabhattacharya.11c@gmail.com>
2024-06-29 10:02:29 +0530

    Merging with branch promit and sankhadeep

--- lib/models/device_categories.dart
+++ lib/models/device_categories.dart
@@ -1,17 +1,18 @@
 class Devices {
+  int device_id;
   String type;
   String imgUrl;
 
-  Devices(this.type, this.imgUrl);
+  Devices(this.device_id,this.type, this.imgUrl);
 }
 
 List<Devices> devices = [
-  Devices('Smartphone', 'assets/images/smartphone.webp'),
-  Devices('Tablet', 'assets/images/tablet.png'),
-  Devices('Laptop', 'assets/images/laptop.png'),
-  Devices('Desktop', 'assets/images/desktop.png'),
-  Devices('Air Conditoner', 'assets/images/ac.png'),
-  Devices('Refridgerator', 'assets/images/fridge.png'),
-  Devices('Toaster', 'assets/images/toaster.png'),
-  Devices('Electric Cooker', 'assets/images/cooker.png'),
+  Devices(0,'Smartphone', 'assets/images/smartphone.webp'),
+  Devices(1,'Tablet', 'assets/images/tablet.png'),
+  Devices(2,'Laptop', 'assets/images/laptop.png'),
+  Devices(3,'Desktop', 'assets/images/desktop.png'),
+  Devices(4,'Air Conditoner', 'assets/images/ac.png'),
+  Devices(5,'Refridgerator', 'assets/images/fridge.png'),
+  Devices(6,'Toaster', 'assets/images/toaster.png'),
+  Devices(7,'Electric Cooker', 'assets/images/cooker.png'),
 ];
