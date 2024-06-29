206414328db9973d79b73ea21d5ea13d1436e5d0
Anurag <hamsen3880@gmail.com>
2024-06-29 09:13:57 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:recyclify/pages/login_page.dart';
 import 'package:recyclify/pages/register_page.dart';
 import 'package:recyclify/services/database.dart';
-import 'package:recyclify/services/auth_service.dart'; 
+import 'package:recyclify/services/auth_service.dart';
 import 'package:recyclify/services/navigation_service.dart';
-import 'package:recyclify/utils.dart'; 
+import 'package:recyclify/utils.dart';
 
 void main() async {
   await setup();
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         textTheme: GoogleFonts.montserratTextTheme(),
       ),
-      initialRoute: _authService.user != null ? "/home": "/login", 
+      initialRoute: _authService.user != null ? "/home" : "/login",
       routes: {
         "/login": (context) => LoginPage(),
         "/home": (context) => HomeScreen(),
